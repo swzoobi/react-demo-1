00105e1173921387c84ec8d9903719308d41dde0
zy <swzoobi@126.com>
2020-05-11 17:50:05 +0800

    第一个react应用

--- src/App.js
+++ src/App.js
@@ -2,25 +2,49 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App() { //函数组件
+  function sayHi(){
+    alert('hi')
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          函数组件 code：<code>function App()</code>
+
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          编辑 <code>src/App.js</code> 并保存，然后重新刷新页面
+        </p>
+        <button onClick={sayHi} >
+          打招呼
+        </button>
+        
       </header>
     </div>
   );
 }
+
+// class App extends React.Component{//对象组件
+
+//   render(){
+//     return (
+//       <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           对象组件 code：<code>class App2 extends React.Component</code>
+//         </p>
+//         <p>
+//           编辑 <code>src/App.js</code> 并保存，然后重新刷新页面
+//         </p>
+        
+//       </header>
+//     </div>
+//     )
+//   }
+// }
 
 export default App;
